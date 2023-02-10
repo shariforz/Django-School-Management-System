8dee6dd2eda04f0e8b7afa5ce16805e2f832cab1
Sharif Orzikulov <orzikulov@gmail.com>
2023-02-10 12:40:14 +0300

    Update urls.py
    Changed for showing files in media folder

--- school_app/urls.py
+++ school_app/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path("staff/", include("apps.staffs.urls")),
     path("finance/", include("apps.finance.urls")),
     path("result/", include("apps.result.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
